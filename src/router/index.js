5aeadcdfec87045c28131f927256461021d052ab
Vashistha_ Pratikshit (pvashistha@deloitte.com) <pvashistha@your.hostname.com>
2023-07-26 15:38:36 +0530

    tailwind added

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import DefaultLayout from "../components/DefaultLayout.vue";
-import GuestLayout from "../components/GuestLayout.vue";
-import Home from "../views/Home.vue";
-import MealsByIngredient from "../views/MealsByIngredient.vue";
-import MealsByLetter from "../views/MealsByLetter.vue";
-import MealsByName from "../views/MealsByName.vue";
-import MealDetails from "../views/MealDetails.vue";
-import Ingredients from "../views/Ingredients.vue";
-
+import Home from "../views/Home.vue"
 const routes = [
   {
     path: "/",
-    component: DefaultLayout,
-    children: [
-      {
-        path: "/",
-        name: "home",
-        component: Home,
-      },
-      {
-        path: "/by-name/:name?",
-        name: "byName",
-        component: MealsByName,
-      },
-      {
-        path: "/by-letter/:letter?",
-        name: "byLetter",
-        component: MealsByLetter,
-      },
-      {
-        path: "/ingredients",
-        name: "ingredients",
-        component: Ingredients,
-      },
-      {
-        path: "/by-ingredient/:ingredient",
-        name: "byIngredient",
-        component: MealsByIngredient,
-      },
-      {
-        path: "/meal/:id",
-        name: "mealDetails",
-        component: MealDetails,
-      },
-    ],
-  },
-  {
-    path: "/guest",
-    component: GuestLayout,
+    name: "Home",
+    component: Home,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history:createWebHistory(),
   routes,
-});
+})
 
 export default router;
